59eba9d3889676b45e361d4b51f74f0011841777
Tariqsharifi <tariqsharifi534@gmail.com>
2026-08-22 10:25:04 +0330

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  SUPABASE_URL: "آدرس-پروژه-اینجا",
+  SUPABASE_URL: "https://jtkjgmpotablnmcbrjkn.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_PM2ydrpkgoM9yQU4uMUeRg_H3cvHQp3",
 
   OCR_FUNCTION_NAME: "ocr-invoice",

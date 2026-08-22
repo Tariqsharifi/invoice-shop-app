bdaa25a1e1d9226bb58bb1ac3c5917523c6a0730
Tariqsharifi <tariqsharifi534@gmail.com>
2026-08-22 11:39:46 +0330

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,5 +3,5 @@ window.APP_CONFIG = {
   SUPABASE_ANON_KEY: "sb_publishable_PM2ydrpkgoM9yQU4uMUeRg_H3cvHQp3",
 
   OCR_FUNCTION_NAME: "ocr-invoice",
-  OCR_MANUAL_FALLBACK: true,
+OCR_MANUAL_FALLBACK: false,
 };

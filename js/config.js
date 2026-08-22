20ba439b284e8221a5e88bb09b04dfa15de422ad
Tariqsharifi <tariqsharifi534@gmail.com>
2026-08-22 04:04:48 +0330

    Delete js/config.js

--- js/config.js
+++
@@ -1,6 +0,0 @@
-window.APP_CONFIG = {
-  SUPABASE_URL: "https://jtkjgmpotablnmcbrjkn.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_PM2ydrpkgoM9yQU4uMUeRg_H3cvHQp3",
-  OCR_MANUAL_FALLBACK: true, // تا وقتی OCR واقعی رو وصل نکردی، true بمونه
-  OCR_FUNCTION_NAME: "ocr-invoice",
-};

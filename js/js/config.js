503481114434beb0e1a2da30ea08d886bc7f2d94
Tariqsharifi <tariqsharifi534@gmail.com>
2026-08-22 09:09:30 +0330

    Delete js directory

--- js/js/config.js
+++
@@ -1,6 +0,0 @@
-window.APP_CONFIG = {
-  SUPABASE_URL: "https://jtkjgmpotablnmcbrjkn.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_PM2ydrpkgoM9yQU4uMUeRg_H3cvHQp3",
-  OCR_MANUAL_FALLBACK: true,
-  OCR_FUNCTION_NAME: "ocr-invoice",
-};
